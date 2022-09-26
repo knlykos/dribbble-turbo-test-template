e128337f3085d2e200e1faefd5544a1c4a3ed32a
Nefi Lopez Garcia <nefi.lopezg@gmail.com>
2022-09-25 20:33:37 -0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'kanban-board-view-turbo-dribbble';
+  avatars = [
+    {image: 'https://avatars.dicebear.com/api/micah/w2.svg', color: 'brown', link: '', name: 'Nefi Lopez Garcia'},
+    {image: 'https://avatars.dicebear.com/api/micah/w2ww.svg', color: 'coral', link: '', name: 'Kimberly Carrillo'},
+    {image: 'https://avatars.dicebear.com/api/micah/w2s.svg', color: 'deeppink', link: '', name: 'Natalia Lopez'},
+    {image: 'https://avatars.dicebear.com/api/micah/w24.svg', color: 'yellow', link: '', name: 'Nathan Benjamin Lopez'}
+  ]
 }
